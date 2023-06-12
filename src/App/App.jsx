058684b0f716734a38a118e026b0bc3f49568f55
olivier.ledanvic <olivier.ledanvic@gmail.com>
2023-06-12 12:09:47 +0200

    Init Button

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '../Button/Button'
 
 function App() {
   return (
     <div className="App">
-      DEMAT breizh !
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
     </div>
   );
 }
---
+++ src/Button/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+/**
+ * 
+ * @param {Object} props 
+ */
+const Button=(props)=>{
+    return <button>Bouton</button>;
+}
+
+export default Button;
