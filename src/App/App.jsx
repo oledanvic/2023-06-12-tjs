1e1483e7260a3feb3eaa844e10e17e2c49922d3a
olivier.ledanvic <olivier.ledanvic@gmail.com>
2023-06-12 16:51:04 +0200

    Utilisation useState

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Button from './components/ui/Button/Button'
 
 function App() {
+  const [counter, setcounter] = useState(0);
   return (
     <div className="App">
-      <div>Olivier - ReactJS</div>
-      <Button className="primary" onClick={(unParamDeLenfant)=>{
-        console.log('fonction envoyee par le parent', unParamDeLenfant);
-      }}><img src='/img/ok.png'/></Button>
-      <Button className="error"><img src='/img/cancel.png'/></Button>
+      La valeur de counter : {counter}
+      <br/>
+      <Button className="error" onClick={()=>{
+        setcounter(counter - 1);
+        console.log(counter);
+      }}>-1</Button>
+      <Button onClick={()=>{
+        setcounter(counter + 1);
+        console.log(counter);
+      }}>+1</Button>
     </div>
   );
 }
